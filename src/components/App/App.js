568f91da0e374d0494c8447997eceb0bb43f9d6f
Sam McCauley <sammccauley34@gmail.com>
2023-07-20 12:13:21 -0500

    added useEffect and fetchGifs function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { UseSelector } from 'react-redux/es/hooks/useSelector';
+import { useDispatch } from 'react-redux';
+
+
 
 function App(props) {
+
+
+useEffect(() => {
+  dispatchEvent({ type: 'FETCH_GIFS' })
+})
+
+function* fetchGifs(action) {
+  try {
+    const fetchresponse = yield axios.get('/')
+    // put = dispatch
+    yield put ({ type: ''})
+  } catch (error) {
+
+  }
+}
+
+
   return (
     <div>
       <h1>Giphy Search!</h1>
