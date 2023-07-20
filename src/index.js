10e08571b96114c57cccfffc46ca053764c0481a
ZakariyeAden <zakariye5417@gmail.com>
2023-07-20 12:01:20 -0500

    setting up redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import axios from "axios";
+// Redux
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import { put, takeLatest } from "@redux-saga/core/effects";
+// Saga middleware
+import createSagaMiddleware from "@redux-saga/core";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const giphyList = (state = [], action) => {
+  switch (action.type) {
+    case "SET_GIPHY":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+// Create the rootSaga generator function
+function* rootSaga() {
+    
+}
+const sagaMiddleware = createSagaMiddleware();
+// Store
+const store = createStore(
+  combineReducers({
+    giphyList,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
+);
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
     <React.StrictMode>
-        <App />
+      <App />
     </React.StrictMode>
+  </Provider>
 );
