65faeb0258243264db2c654f740bb80cfc32c957
Zach Garrison <zgedina@yahoo.com>
2023-07-20 12:31:52 -0500

    gif card and search page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import SearchPage from '../SearchPage/SearchPage';
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <SearchPage />
     </div>
   );
 }
---
+++ src/components/SearchPage/SearchPage.jsx
@@ -0,0 +1,17 @@
+import GifCard from "../GifCard/GifCard";
+import { Button, TextField } from "@mui/material";
+
+
+
+function SearchPage() {
+  return (
+    <>
+    <div><TextField id="filled-basic" label="Search a GIF" variant="filled" style={{display:"flex", justifyContent:'center', width:'300px'}} />
+         <Button variant="contained" style={{display:'flex',margin:'20px'} }>Search Giphy</Button></div>
+         <Button variant="contained" style={{display:'flex',margin:'20px'}}>Show Favorites</Button>
+         <GifCard />
+         </>
+  )
+}
+
+export default SearchPage
