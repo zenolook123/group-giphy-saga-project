36aecccbd2fd52c38ef37b7d316c497269e3019a
ZakariyeAden <zakariye5417@gmail.com>
2023-07-20 15:23:57 -0500

    update

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const axios = require('axios');
 
 // Route 
-https://api.giphy.com/v1/gifs/trending?api_key=RoJ4O0Lh2EQOZ6kh6gAlucu5lSMtPJP8
 router.get('/', (req, res) => {
     axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}`)
     .then( response => {
